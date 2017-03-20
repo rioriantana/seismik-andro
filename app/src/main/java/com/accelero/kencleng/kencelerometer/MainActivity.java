2604a1e9ce892ca9f98ff70d3df92f4bf5b7227b
Rio Riantana <rioriantana@yahoo.com>
2017-03-20 10:26:27 +0700

    oke

--- app/src/main/java/com/accelero/kencleng/kencelerometer/MainActivity.java
+++ app/src/main/java/com/accelero/kencleng/kencelerometer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.accelero.kencleng.kencelerometer;
 
 import android.hardware.Sensor;
+import android.net.Uri;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.SensorEvent;
@@ -8,49 +10,81 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private long lastUpdate;
     private float x;
     private float y;
+    private Float[] baris;
+    public Integer i = 0;
+    public String yoyoi = "yoi";
     public TextView sumbuX;
+    public TextView sumbuXX;
     private float z;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        sumbuX = (TextView) findViewById(R.id.sumbuX);
+
+        sumbuXX = (TextView) findViewById(R.id.sumbuXX);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         lastUpdate = System.currentTimeMillis();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event){
-        if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
             getAccelerometer(event);
         }
     }
 
-    private void getAccelerometer(SensorEvent event){
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void getAccelerometer(SensorEvent event) {
         float[] value = event.values;
 
         x = value[0];
         y = value[1];
         z = value[2];
 
+        String nilaiX = Float.toString(x);
+
+        sumbuX.setText(nilaiX);
+        baris = new Float[0];
+        baris[i] = x;
+        i++;
         lastUpdate = event.timestamp;
+        //String[] arrayString  =  Arrays.copyOf(baris, baris.length, String[].class);
+
+       // sumbuXX.append(arrayString[i]);
     }
 
-    @Override
-    public void onAccuratyChanged(Sensor sensor, int accuracy){
-
-    }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
 
         sensorManager.registerListener(this,
@@ -59,8 +93,44 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
